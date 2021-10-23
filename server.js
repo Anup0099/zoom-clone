1ef73e738c749c6493a2e023acaa517e510e8c73
anup <anuppal602@gmail.com>
2021-10-23 21:02:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").Server(app);
+const {v4: uuidv4} = require("uuid");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("room");
+});
+
+
+
+
+
+
+
+
+
+
+server.listen(3030);
